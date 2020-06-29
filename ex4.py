cad8ea098ef86a8d53384e84b416d652f8fc54c9
Creative Mus <mastour.mustapha@gmail.com>
2020-06-29 08:52:16 +0200

    finish algo

--- ex4.py
+++ ex4.py
@@ -7,21 +7,44 @@
 
 def planning(cours):
 
-    cours.sort() # On trie notre tableau de cours par ordre croissant
+    cours.sort()  # On trie notre tableau de cours par ordre croissant
 
-    theta = []
+    planning = []
     duree_max = 4
+
+    return planning_rec(cours, planning, duree_max)
+
+
+def planning_rec(cours, planning, duree_max):
+
+    if len(cours) == 0:
+        return planning
+
+    room = []
+    to_remove = []
     current_duree = 0
 
     for element in cours:
+            # print(element + current_duree)
         if element + current_duree <= duree_max:
-            theta.append(element)
+            room.append(element)
+            to_remove.append(element)
             current_duree += element
 
-    return theta
+    if len(to_remove) > 0:
+        for element in to_remove:
+            cours.remove(element)
+
+    print(len(cours))
+
+    planning.append(room)
+
+    return planning_rec(cours, planning, duree_max)
+
+
 
 def main():
-    cours = [3, 1.5, 2.5, 3, 0.75]
+    cours = [ 1.5, 2.5, 3, 0.75]
     print(planning(cours))
 
 if __name__ == '__main__':
