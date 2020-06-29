719c6669e0e2c4586ff08b74fa906d0ce37d21d7
Creative Mus <mastour.mustapha@gmail.com>
2020-06-29 09:09:43 +0200

    add comment

--- ex4.py
+++ ex4.py
@@ -10,7 +10,7 @@ def planning(cours):
     cours.sort()  # On trie notre tableau de cours par ordre croissant
 
     planning = []
-    duree_max = 4
+    duree_max = 4 # nombre d'heure maximum ou placer nos cours
 
     return planning_rec(cours, planning, duree_max)
 
@@ -24,27 +24,24 @@ def planning_rec(cours, planning, duree_max):
     to_remove = []
     current_duree = 0
 
-    for element in cours:
-            # print(element + current_duree)
-        if element + current_duree <= duree_max:
+    for element in cours: ## On ajoute tous les cours pouvant entrer dans la meme salle tout en respectant la durée max
+        if element + current_duree <= duree_max: 
             room.append(element)
             to_remove.append(element)
             current_duree += element
 
     if len(to_remove) > 0:
-        for element in to_remove:
+        for element in to_remove: ## On supprime les elements deja utilsés
             cours.remove(element)
-
-    print(len(cours))
 
     planning.append(room)
 
-    return planning_rec(cours, planning, duree_max)
+    return planning_rec(cours, planning, duree_max) 
 
 
 
 def main():
-    cours = [ 1.5, 2.5, 3, 0.75]
+    cours = [1.5, 2.5, 3, 0.75]
     print(planning(cours))
 
 if __name__ == '__main__':
