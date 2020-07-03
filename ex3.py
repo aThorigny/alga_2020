c491af2273c4e493113e08c08a0d2df53af56e2b
Creative Mus <mastour.mustapha@gmail.com>
2020-07-03 17:09:44 +0200

    add algo ex 3

--- ex3.py
+++ ex3.py
@@ -1,7 +1,43 @@
 # EXERCICE 3
 
 #
-# Question 1
+# Question 5
 #
 
-#
+def Lk(string):
+
+    if len(string) == 1:
+        return [0]
+
+    stringSize = len(string)
+
+    arrayPrefixe = [0] * stringSize
+
+    count = 0
+    i = 1
+
+    while i < stringSize:
+
+        if string[i] == string[count]: 
+            count += 1
+            arrayPrefixe[i] = count
+            i += 1
+
+        elif count > 0:
+            count = arrayPrefixe[count - 1]
+
+        else:
+            arrayPrefixe[i] = count
+            i += 1
+
+    return arrayPrefixe
+
+
+
+def main():
+    print(Lk('ABABCABABABC'))
+    print(Lk('MUSALANMUS'))
+    print(Lk('ALANMUSALAN'))
+
+if __name__ == '__main__':
+    main()
