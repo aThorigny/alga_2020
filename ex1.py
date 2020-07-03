d84853f000bdd0101c0965b6f5d0a184577eb98d
Alan Thorigny <thorigny.alan@gmail.com>
2020-07-03 15:45:26 +0200

    implemented get_substr_palindrome

--- ex1.py
+++ ex1.py
@@ -77,7 +77,39 @@ def get_longest_palindrome_dyn(X):
 
 
 def get_substr_palindrome(X):
-    pass
+    length = len(X)
+
+    table = [[0 for x in range(length)] for y in range(length)]
+
+    max_length = 1
+    i = 0
+    while i < length:
+        table[i][i] = True
+        i = i + 1
+
+    start = 0
+    i = 0
+    while i < length - 1:
+        if X[i] == X[i + 1]:
+            table[i][i + 1] = True
+            start = i
+            max_length = 2
+        i = i + 1
+
+    k = 3
+    while k <= length:
+        i = 0
+        while i < (length - k + 1):
+            j = i + k - 1
+            if table[i + 1][j - 1] and X[i] == X[j]:
+                table[i][j] = True
+                if k > max_length:
+                    start = i
+                    max_length = k
+            i = i + 1
+        k = k + 1
+
+    return X[start: start + max_length]
 
 
 def main():
@@ -90,7 +122,8 @@ def main():
     print(get_longest_palindrome('ALAN'))
     print(get_longest_palindrome('BANANA'))
     print(get_longest_palindrome('SOUEARRROUSRRAE'))
-    print('ok')
+    print(get_longest_palindrome("ABCAB"))
+    print(get_substr_palindrome("ABCAB"))
     print(get_longest_palindrome_dyn('atezrtezeaaatsdfsttatez'))
     print(get_longest_palindrome('atezrtezeaaatsdfsttatez'))
 
