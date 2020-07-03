232fd3df458724b50f28385a6ccb2793fb997114
Alan Thorigny <thorigny.alan@gmail.com>
2020-07-03 15:54:46 +0200

    reimplemented get_substr_palindrome

--- ex1.py
+++ ex1.py
@@ -82,32 +82,24 @@ def get_substr_palindrome(X):
     table = [[0 for x in range(length)] for y in range(length)]
 
     max_length = 1
-    i = 0
-    while i < length:
+    for i in range(length):
         table[i][i] = True
-        i = i + 1
 
     start = 0
-    i = 0
-    while i < length - 1:
+    for i in range(length - 1):
         if X[i] == X[i + 1]:
             table[i][i + 1] = True
             start = i
             max_length = 2
-        i = i + 1
 
-    k = 3
-    while k <= length:
-        i = 0
-        while i < (length - k + 1):
+    for k in range(3, length):
+        for i in range(length - k + 1):
             j = i + k - 1
             if table[i + 1][j - 1] and X[i] == X[j]:
                 table[i][j] = True
                 if k > max_length:
                     start = i
                     max_length = k
-            i = i + 1
-        k = k + 1
 
     return X[start: start + max_length]
 
